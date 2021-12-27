e6625826f07260b2091c18bac528707ce61a4e99
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-27 18:19:29 +0600

    login authentication done

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const upload = require('./utils/multer');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const objectId = require('mongodb').ObjectId;
-const jwt = require('jsonwebtoken');
+const fileUpload = require('express-fileupload');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mkcgo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,20 +22,20 @@ client.connect(err => {
 
     app.post('/signup', async (req, res) => {
         try{
-            const userInfo = req.body;
-            const userExist = await usersCollection.findOne({ email: userInfo.email});
+            const { name, email, password } = req.body;
+            const userExist = await usersCollection.findOne({ email: email});
 
             if(userExist){
-                return res.status(422).json({ error: 'User already exists' });
+                res.status(200).send({ error: 'User already exists' });
             }
            else{
-                const hashPassword = await bcrypt.hash(userInfo.password, 12);
-                const result = await usersCollection.insertOne({ ...userInfo, password: hashPassword });
+                const hashPassword = await bcrypt.hash(password, 12);
+                const result = await usersCollection.insertOne({ name, email, password: hashPassword });
                 res.status(201).send(result);
            }
         }
         catch(err) {
-            res.send(err);
+            res.status(500).send(err);
         }
     })
 
@@ -47,19 +48,18 @@ client.connect(err => {
                 if(validPassword){
                     const token = await jwt.sign({ _id: user._id }, `${process.envJWT_SECRET}`);
                     const data = { token, user };
-                    console.log(data);
-                    res.status(201).send(data);
+                    res.status(200).send(data);
                 }
                 else{
-                    res.status(400).send({error: "Invalid Password"});
+                    res.status(200).send({error: "Password Incorrect"});
                 }
             }
             else{
-                res.status(401).send({error: "User does not exist"});
+                res.status(200).send({error: "User does not exist"});
             }
         }
         catch (err) {
-            res.send(err);
+            res.status(500).send(err);
         }
     })
 
